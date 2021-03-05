53f372149377bb556b07eb9c7ef20bf5a9b43d46
unknown <gr.s.joe@gmail.com>
2021-03-05 18:03:53 +0500

    Add coments for generic api

--- src/api/generic.api.js
+++ src/api/generic.api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
-
+// api. Проксируется на http://localhost:3001. настройка в vue.config.js и package.json
 const API_URL = "/";
 let endPoints = [`Email`, `Tel`, `Ip`, `Nickname`, `FullName`];
 
 // делаем запрос к api
 const fetchData = (url, method, entity) => {
+  // получение данных
   const getData = async (urls) => {
     try {
       const response = await axios[method](`${API_URL}${urls} `);
+      // получаем данные
       return {
         success: true,
         data: response.data,
@@ -15,6 +17,7 @@ const fetchData = (url, method, entity) => {
       };
     } catch (error) {
       console.log(error);
+      // обрабатываем ошибку если запрос не к api не сработал
       return {
         success: false,
         data: undefined,
@@ -25,8 +28,9 @@ const fetchData = (url, method, entity) => {
       };
     }
   };
-
+  // проверка на распознание типа сущности
   if (typeof url !== "string") {
+    // получаем данные на фсе endpoints
     try {
       return url.map(getData);
     } catch (e) {
@@ -35,6 +39,7 @@ const fetchData = (url, method, entity) => {
     }
   } else {
     try {
+      // получаем данные на конкретный endpoint
       return getData(url);
     } catch (e) {
       console.log(e);
@@ -42,35 +47,42 @@ const fetchData = (url, method, entity) => {
     }
   }
 };
-
+// запрос на все endpoits
 const fetchAllData = (endPoints, method, entity) => {
-  return Promise.allSettled(fetchData(endPoints, method, entity))
-    .then((res) => {
-      return Promise.allSettled(
-        res.map((resObj) => {
-          return resObj.value.data;
-        })
-      );
-    })
-    .then((resObjData) => {
-      return {
-        success: true,
-        data: [...new Set([].concat(...resObjData))],
-        entity,
-      };
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+  return (
+    Promise.allSettled(fetchData(endPoints, method, entity))
+      .then((res) => {
+        return Promise.allSettled(
+          res.map((resObj) => {
+            return resObj.value.data;
+          })
+        );
+      })
+      // получаем данный
+      .then((resObjData) => {
+        return {
+          success: true,
+          data: [...new Set([].concat(...resObjData))],
+          entity,
+        };
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+  );
 };
 
 export const request = async ({ url, method, entity }) => {
   try {
+    // если тип сущности известен делаем запрос к api
     if (entity.type !== "any") {
+      // url строка
       const response = await fetchData(url, method, entity);
 
       return response;
-    } else {
+    } // если тип сущности не известен делаем запросы все endpoints
+    else {
+      // endPoints массив всех endpoints
       const result = await fetchAllData(endPoints, method, entity);
 
       return result;
--- src/main.js
+++ src/main.js
@@ -6,15 +6,9 @@ import "./style/main.scss";
 import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
 import router from "./router";
-import { ValidationProvider, ValidationObserver } from "vee-validate";
-
-// Register it globally
-// main.js or any entry file.
-Vue.component("ValidationProvider", ValidationProvider);
-Vue.component("ValidationObserver", ValidationObserver);
 
 UIkit.use(Icons);
-// Vue.use(uikit);
+
 Vue.config.productionTip = false;
 
 new Vue({
